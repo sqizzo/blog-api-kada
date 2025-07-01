512cfcc6700c46bdea84cabc831e5a25aca55506
sqizzo <baihaqimnb@gmail.com>
2025-07-01 15:04:06 +0700

    fix

---
+++ data/posts.js
@@ -0,0 +1,27 @@
+let posts = [
+  {
+    id: 1,
+    title: "First Note",
+    content: "This is the first note written by Annie.",
+    author: "annie",
+  },
+  {
+    id: 2,
+    title: "The second was written by Bob.",
+    content: "Hello, this is Bob.",
+    author: "bob",
+  },
+  {
+    id: 3,
+    title: "Bob wrote again.",
+    content: "This is the second note written by Bob.",
+    author: "bob",
+  },
+  {
+    id: 4,
+    title: "Chris wrote a note.",
+    content: "Hello, this is Chris.",
+    author: "chris",
+  },
+];
+module.exports = posts;
